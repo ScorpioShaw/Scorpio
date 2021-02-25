0a330ed7dad2fe57f07a5d235cde93568d466b33
ScorpioShaw <72494790+ScorpioShaw@users.noreply.github.com>
2021-02-25 16:45:50 +0800

    Add files via upload

---
+++ 小学数学 N 合一.cpp
@@ -0,0 +1,91 @@
+# include <bits/stdc++.h>
+int main (void)
+{
+	while (1)
+	{
+		int question ;
+		scanf ("%d",&question) ;
+		
+		if (question == 1)
+		{
+			printf ("I love Luogu!") ;
+		} 
+		
+		if (question == 2)
+		{
+			printf ("6 4") ;
+		}
+		
+		if (question == 3)
+		{
+			printf ("3\n") ;
+			printf ("12\n") ;
+			printf ("2") ;
+		}
+		
+		if (question == 4)
+		{
+			printf ("166.667") ;
+		}
+		
+		if (question == 5)
+		{
+			printf ("15") ;
+		}
+		
+		if (question == 6)
+		{
+			printf ("%.4f",sqrt(117)) ;
+		}
+		
+		if (question == 7)
+		{
+			printf ("110\n") ;
+			printf ("90\n") ;
+			printf ("0") ;
+		}
+		
+		if (question == 8)
+		{
+			printf ("%.4f\n",2*5*3.141593) ;
+			printf ("%.4f\n",3.141593*25) ;
+			printf ("%.3f",1.333333*3.141593*125) ;
+		}
+		
+		if (question == 9)
+		{
+			printf ("22") ;
+		}
+		
+		if (question == 10)
+		{
+			printf ("9") ;
+		}
+		
+		if (question == 11)
+		{
+			printf ("33.333") ;
+		}
+		
+		if (question == 12)
+		{
+			printf ("13\n") ;
+			printf ("R") ;
+		}
+		
+		if (question == 13)
+		{
+			float a = pow (1.333333*3.141593*1064, 0.333333) ;
+			printf ("%.0f",a) ;
+		}
+		
+		if (question == 14)
+		{
+			printf ("50") ;
+		}
+		printf ("\ncycle\n") ;
+	}
+	
+	
+	return 0 ;
+ } 
