98a7309aceb5aaa44410b8e9a2bb027811a75aab
ScorpioShaw <72494790+ScorpioShaw@users.noreply.github.com>
2021-03-16 07:21:00 -0800

    Add files via upload

---
+++ 九九乘法表C++.cpp
@@ -0,0 +1,21 @@
+# include <bits/stdc++.h>
+using namespace std ;
+int main (void)
+{
+//	while (1)
+//	{
+        int a = 10 ;
+        cout << a << "wuhu" << endl ;
+         
+		for (int a = 1; a < 10; a++)
+		{
+			for (int b = 1; b <= a; b++)
+		    {
+			    cout << a << " * " << b << " = " << a*b << "   " ; 
+	 	    }
+			cout << endl ; 
+		}	
+//		cout << "\ncycle\n" ;
+//	}
+	return 0 ;
+ } 
